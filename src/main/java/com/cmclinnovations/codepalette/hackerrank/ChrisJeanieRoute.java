a3b0ed526d16697c62131e55bd6b7f989cf15d28
Christopher Stavrou <christopherstavrou@gmail.com>
2018-09-13 22:45:47 +0100

    hackerrank: boilerplate code for writing a solution to Jeanie's Route problem

---
+++ src/main/java/com/cmclinnovations/codepalette/hackerrank/ChrisJeanieRoute.java
@@ -0,0 +1,29 @@
+package com.cmclinnovations.codepalette.hackerrank;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ChrisJeanieRoute {
+
+    /*
+     * Complete the jeanisRoute function below.
+     */
+    static int jeanisRoute(int[] k, int[][] roads) {
+        int i = 0;
+
+        /*
+         * Write your code here.
+         */
+        return i;
+    }
+
+    @Test
+    public void Test_1(){
+        int[] k = {};
+        int[][] roads = {{}, {}};
+        int expected = 0;
+        int actual = jeanisRoute(k, roads);
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
