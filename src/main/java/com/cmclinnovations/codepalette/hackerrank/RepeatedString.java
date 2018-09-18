3805326c49573d357b3e98957a7bbbcf06412197
Christopher Stavrou <christopherstavrou@hotmail.com>
2018-09-18 21:24:17 +0100

    hackerrank: added solution for repeated string

--- src/main/java/com/cmclinnovations/codepalette/hackerrank/RepeatedString.java
+++ src/main/java/com/cmclinnovations/codepalette/hackerrank/RepeatedString.java
@@ -3,14 +3,44 @@ package com.cmclinnovations.codepalette.hackerrank;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 public class RepeatedString {
     // Complete the repeatedString function below.
     static long repeatedString(String s, long n) {
-            return n /s.length();
+        long result = 0;
+
+        String combined = "";
+        int i = 0, slength = 0;
+
+        while (slength <= n) {
+            combined += s;
+            slength = combined.length();
+            i++;
+        }
+
+        char[] chars = combined.toCharArray();
+
+        int ii = 0;
+        while (ii < n) {
+            char c = chars[ii];
+            if (c == 'a') {
+                result ++;
+            }
+            ii++;
+        }
+//        String repeatedString = LongStream.range(0, n).mapToObj(i -> s).collect(Collectors.joining()).substring(0,(int)n);
+//        long count = Stream.of(repeatedString.toCharArray()).filter(c-> Objects.equals(c,'a')).count();
+//        return n /s.length();
+
+        return result;
     }
 
     @Test
-    public void Test_0(){
+    public void Test_0() {
         // aba aba aba a
 
         final String s = "aba";
